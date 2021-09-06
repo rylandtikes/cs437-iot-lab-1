5700878b6a24a7c7ec1be13828946d3d634994f0
Charles Stolz <charles.stolz@protonmail.com>
2021-09-06 15:40:37 -0400

    adding tensor flow test and install script

---
+++ scripts/tflite_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install TensorFlow Lite interpreter on raspberry pi
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install python3-tflite-runtime
