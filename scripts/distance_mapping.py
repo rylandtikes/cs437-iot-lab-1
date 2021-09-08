97b7fc224d113a30252e1a4c610fe10596764846
Thomas Kalnik <kalnik.thomas@gmail.com>
2021-09-08 17:40:44 -0500

    working implementation of map for first row

--- scripts/distance_mapping.py
+++ scripts/distance_mapping.py
@@ -29,14 +29,14 @@ thres = 25
 
 def test_servo(start, end):
     for angle in range(start, end + 10, 5):
-        print(f"testing servo at {angle}")
+        #print(f"testing servo at {angle}")
         servo.set_angle(angle)
         distance = get_distance()
         distances.update([(angle, distance)])
 
 def get_distance():
     distance = us.get_distance()
-    #print(f"Distance is {distance}")
+    print(f"Distance is {distance}")
     return distance
 
 def interpolate(v1,v2,t):
@@ -44,15 +44,18 @@ def interpolate(v1,v2,t):
     return t
 
 def make_map():
-    for i in distances:
-        dist = distances[i]
-        print("distance: ", dist)
+    for k,v in distances.items():
+        ang = k
+        dist = v 
+        print(ang, ": ", dist)
         if(dist > thres):
-            area_map[i] = 1
+            area_map[0][ang] = 1
         else: 
-            area_map[i] = 0
-    return print(area_map)
+            area_map[0][ang] = 0
+    return print(area_map[0:3])
 
 if __name__ == "__main__":
+    #TODO: Add logic to call the make_map function each time the car moves
+    #while(True):
     test_servo(-90, 85)
     make_map()
